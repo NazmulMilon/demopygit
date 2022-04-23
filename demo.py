37dec750ef8b1399a406505500575e74fd382f09
nazmulmilon <milonsarker73@gmail.com>
2022-04-23 14:38:08 +0600

    create class and function for add and sub numbers

--- demo.py
+++ demo.py
@@ -1,5 +1,27 @@
-print("Hello World!!")
+class A:
+    def sum(a, b, c):
+        add = a + b + c
+        # return add
 
-print("This is Md. Milon Sarker")
+    def sub(self, m, n):
+        diff = m-n
+        return diff
 
-print("Hey I have done successfully")
+
+m1, m2 = map(int, input("Enter numbr: ").split())
+obj = A()
+x = obj.sub(m1, m2)
+
+print(x)
+
+
+'''
+def sum(a,b,c):
+    add = a+b+c
+    return add
+
+
+x, y, z = map(int, input("Enter number: ").split())
+r = sum(x, y, z)
+print(r)
+'''
